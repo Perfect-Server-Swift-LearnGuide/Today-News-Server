280ac15c64011b6de7e4e8623abf4b1f27749487
lovemomo <928627819@qq.com>
2017-06-30 22:49:30 +0800

    update perfectLibs

--- Package.swift
+++ Package.swift
@@ -10,7 +10,14 @@ let package = Package(
         Target(name: "TNRoute", dependencies: ["TNHandler", "TNCommon"])
     ],
 	dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-        .Package(url:"https://github.com/PerfectlySoft/Perfect-MongoDB.git", versions: Version(0,0,0)..<Version(10,0,0))
+        .Package(
+            url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
+            majorVersion: 2),
+        .Package(
+            url:"https://github.com/PerfectlySoft/Perfect-MongoDB.git",
+            majorVersion: 2, minor: 0),
+        .Package(
+            url:"https://github.com/PerfectlySoft/PerfectLib.git",
+            majorVersion: 2, minor: 0)
     ]
 )
