24fc39b7482ed24673cfb73270a20ef5e1de2cbf
lovemomo <928627819@qq.com>
2018-01-12 17:11:34 +0800

    daupte

--- Sources/DataBase/DB.swift
+++ Sources/DataBase/DB.swift
@@ -26,8 +26,8 @@ public class DB {
         
         /// 通过默认的端口连接MongoDB
         
-        self.client = try! MongoClient(uri: "mongodb://lovemo:perfect_swift@" + database.hostname + ":" + database.dbport)
- 
+        self.client = try! MongoClient(uri: "mongodb://lovemo:perfect_swift@" + database.hostname + ":" + database.dbport + "/" + "today_news")
+ print("mongodb://lovemo:perfect_swift@" + database.hostname + ":" + database.dbport)
         /// DataBase
         self.db = self.client.getDatabase(name: db)
         
