d6a1984054c502105866c3f6a5753e83fa5ef726
lovemomo <928627819@qq.com>
2018-01-12 19:07:02 +0800

    daupte

--- Sources/DataBase/DB.swift
+++ Sources/DataBase/DB.swift
@@ -28,7 +28,7 @@ public class DB {
         
         self.client = try! MongoClient(uri: "mongodb://lovemo:perfect_swift@" + database.hostname + ":" + database.dbport + "/" + db)
 
-        fail(self.collection)
+        fail(self.client)
         
         /// DataBase
         self.db = self.client.getDatabase(name: db)
