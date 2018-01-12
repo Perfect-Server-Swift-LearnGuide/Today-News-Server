28aaf4370a35f7a3cd8c01ea71de7d8dabb05598
lovemomo <928627819@qq.com>
2018-01-12 16:56:15 +0800

    update

--- Sources/DataBase/DB.swift
+++ Sources/DataBase/DB.swift
@@ -35,7 +35,7 @@ public class DB {
     /// init collection
     public func collection(name: String) -> Self {
         self.collection = self.db.getCollection(name: database.dbprefix + name)
-        print(self.collection.getLastError())
+        print(self.collection!.getLastError())
         return self
     }
     
