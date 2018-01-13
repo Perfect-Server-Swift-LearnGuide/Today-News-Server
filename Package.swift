ff62a257124065e36ad8c27b9f641acab968d616
lovemomo <928627819@qq.com>
2018-01-13 13:25:51 +0800

    update db

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ let package = Package(
 	name: "Today-News-Server",
 	targets: [
 
-        Target(name: "Application", dependencies: ["Server"]),
-        Target(name: "Common", dependencies: []),
-        Target(name: "Config", dependencies: []),
-        Target(name: "Model", dependencies: ["Config","Common","DataBase"]),
-        Target(name: "DataBase", dependencies: ["Config"]),
-        Target(name: "Server", dependencies: ["Route","Config"]),
-        Target(name: "Handler", dependencies: ["Common", "DataBase","Config","Model"]),
-        Target(name: "Route", dependencies: ["Handler", "Common"])
+//        Target(name: "Application", dependencies: ["Server"]),
+//        Target(name: "Common", dependencies: []),
+//        Target(name: "Config", dependencies: []),
+//        Target(name: "Model", dependencies: ["Config","Common","DataBase"]),
+//        Target(name: "DataBase", dependencies: ["Config"]),
+//        Target(name: "Server", dependencies: ["Route","Config"]),
+//        Target(name: "Handler", dependencies: ["Common", "DataBase","Config","Model"]),
+//        Target(name: "Route", dependencies: ["Handler", "Common"])
     ],
 	dependencies: [
         .Package(
