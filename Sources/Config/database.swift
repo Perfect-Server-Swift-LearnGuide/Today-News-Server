5a1f7e1068ca040943b81338495a8ab78fefa345
lovemo@ubuntu_server <lovemo_ubuntu_server@172.16.153.128>
2018-01-13 12:07:37 +0800

    update

--- Sources/Config/database.swift
+++ Sources/Config/database.swift
@@ -1,21 +1,21 @@
 /// 数据库
 public struct database {
     
-    struct today_news {
+     public struct today_news {
         /// 数据库服务器地址
-        static let hostname = "182.92.83.11"
+        public static let hostname = "182.92.83.11"
         /// 用户名
-        static let username = "lovemo"
+        public static let username = "lovemo"
         /// 密码
-        static let password = "perfect_swift"
+        public static let password = "perfect_swift"
         /// 数据库名
-        static let database =  "today_news"
+        public static let database =  "today_news"
         /// 数据库表前缀
-        static let dbprefix = ""
+        public static let dbprefix = ""
         /// 数据库端口号
-        static let dbport = "27017"
+        public static let dbport = "27017"
         /// 数据库连接地址
-        static let connection {
+        public static var connection: String {
             return "mongodb://" +
                 today_news.username  + ":" +
                 today_news.password + "@" +
