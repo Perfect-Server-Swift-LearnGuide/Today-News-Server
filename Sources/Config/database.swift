0db8f59a992f99de75a069b20a9e8877fc48eb7a
lovemomo <928627819@qq.com>
2018-01-13 10:46:49 +0800

    update

--- Sources/Config/database.swift
+++ Sources/Config/database.swift
@@ -2,17 +2,17 @@
 public struct database {
 
     /// 数据库服务器地址
-    public static let hostname                      =                   "182.92.83.11"
+     static let hostname                      =                   "182.92.83.11"
     /// 用户名
-    public static let username                      =                   "lovemo"
+     static let username                      =                   "lovemo"
     /// 密码
-    public static let password                       =                   "perfect_swift"
+     static let password                       =                   "perfect_swift"
     /// 数据库名            
-    public static let database                       =                    "today_news"
+     static let database                       =                    "today_news"
     /// 数据库表前缀
-    public static let dbprefix                          =                   ""
+     static let dbprefix                          =                   ""
     /// 数据库端口号
-    public static let dbport                             =                   "27017"
+     static let dbport                             =                   "27017"
 
     
 }
